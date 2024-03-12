3ca82b3bf975a364acdd44016252bf5de78cc801
ArturoTF <arturotroyano@gmail.com>
2024-03-12 18:17:23 +0100

    add onEnable onDisable

--- src/miPlugin/MiPlugin.java
+++ src/miPlugin/MiPlugin.java
@@ -1,6 +1,7 @@
 package miPlugin;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -9,7 +10,7 @@ public class MiPlugin extends JavaPlugin{
 	PluginDescriptionFile pdFile = getDescription();
 	
 	public String version = pdFile.getVersion();
-	public String nombre = pdFile.getName();
+	public String nombre = ChatColor.YELLOW+"["+ChatColor.BLUE+pdFile.getName()+ChatColor.YELLOW+"]";
 
 	public static void main(String[] args) {
 
@@ -17,8 +18,13 @@ public class MiPlugin extends JavaPlugin{
 	
 	public void onEnable() {
 		Bukkit.getConsoleSender().sendMessage(
-				"El Plugin "+nombre+" ha sido activado (version: "+version+")"
+				ChatColor.DARK_PURPLE+"El Plugin "+nombre+ChatColor.WHITE+" ha sido activado (version: "+ChatColor.RED+version+ChatColor.WHITE+")"
 		);
 	}
-
+	
+	public void onDisable() {
+		Bukkit.getConsoleSender().sendMessage(
+				ChatColor.DARK_PURPLE+"El Plugin "+nombre+ChatColor.WHITE+" ha sido desactivado (version: "+ChatColor.RED+version+ChatColor.WHITE+")"
+		);
+	}
 }
