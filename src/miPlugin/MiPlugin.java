ce9d66247ace951f8f51f6c17555d704684427f6
ArturoTF <arturotroyano@gmail.com>
2024-03-12 17:03:40 +0100

    primer commit

--- src/miPlugin/MiPlugin.java
+++ src/miPlugin/MiPlugin.java
@@ -1,12 +1,24 @@
 package miPlugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MiPlugin extends JavaPlugin{
+	
+	PluginDescriptionFile pdFile = getDescription();
+	
+	public String version = pdFile.getVersion();
+	public String nombre = pdFile.getName();
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	}
+	
+	public void onEnable() {
+		Bukkit.getConsoleSender().sendMessage(
+				"El Plugin "+nombre+" ha sido activado (version: "+version+")"
+		);
 	}
 
 }
